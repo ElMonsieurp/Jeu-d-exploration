2e94cd86ad88a79fd8ab0a42ddc5fd75870901dc
Stévenn Guégan <59653320+ElMonsieurp@users.noreply.github.com>
2020-02-03 10:02:04 +0100

    Add files via upload
    Choix de la direction par un choix écrit avec des lettres.

--- jeuexplo.c
+++ jeuexplo.c
@@ -1,29 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int grotte;
-int montagne;
-int village;
-int choixd;
+char choixd[20];
+int savechoix;
+
+char village[8];
+
+char montagne[9];
+
+char grotte[7];
 
 int main() {
-choixd=0;
+  village[0]='v';
+  village[1]='i';
+  village[2]='l';
+  village[3]='l';
+  village[4]='a';
+  village[5]='g';
+  village[6]='e';
+  village[7]='\0';
+
+  montagne[0]='m';
+  montagne[1]='o';
+  montagne[2]='n';
+  montagne[3]='t';
+  montagne[4]='a';
+  montagne[5]='g';
+  montagne[6]='n';
+  montagne[7]='e';
+  montagne[8]='\0';
+
+  grotte[0]='g';
+  grotte[1]='r';
+  grotte[2]='o';
+  grotte[3]='t';
+  grotte[4]='t';
+  grotte[5]='e';
+  grotte[6]='\0';
+  savechoix=0;
+
 printf("==================\n Bienvenue aventurier! \n==================\n");
-printf("Ou souhaitez vous aller? \n 1= Village \n 2= Montagne \n 3= grotte \n");
-while (choixd!=1 || choixd!=2 || choixd!=3) {
-scanf("%d",&choixd);
-if (choixd==1) {
+
+printf("Ou souhaitez vous aller? \n");
+
+printf("village? \n");
+
+printf("grotte? \n");
+
+printf("montagne? \n");
+while (savechoix==0) {
+scanf("%s",&choixd);
+if (strcmp(choixd, village) == 0) {
 printf("Vous allez au village... \n ... \nVous etes arriver au village");
+savechoix=1;
 }
-if (choixd==2) {
+else if (strcmp(choixd, montagne) == 0) {
 printf("Vous allez a la montagne... \n ... \nVous etes arriver au pied de la montagne attention!");
+savechoix=1;
 }
-if (choixd==3) {
+else if (strcmp(choixd, grotte) == 0) {
 printf("Vous allez dans une grotte... \n ... \nVous etes arriver dans la grotte...");
+savechoix=1;
 }
-if (choixd>3) {
-printf("Veuillez choisir un autre numero...");
-choixd=0;
+else  {
+printf("Veuillez choisir un autre endroit...");
 }
 }
 }
